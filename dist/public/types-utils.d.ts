d93f7acf5113e5ab7697839124a49f0b28f2a1d6
ntolstikova <nataliia@hejalbert.se>
2023-06-28 11:42:22 +0100

    fix: dist - not possible to answer some keyboard required questions

--- dist/public/types-utils.d.ts
+++ dist/public/types-utils.d.ts
@@ -1,11 +1,11 @@
-/* 0.69.9 *//**
+/* 0.89.4 *//**
  * @internal
  */
-declare type Filter<T, Cond, U extends keyof T = keyof T> = {
+type Filter<T, Cond, U extends keyof T = keyof T> = {
     [K in U]: T[K] extends Cond ? K : never;
 }[U];
 /**
  * @internal
  */
-export declare type Keys<T> = Filter<T, (...args: any[]) => any> & string;
+export type Keys<T> = Filter<T, (...args: any[]) => any> & string;
 export {};
